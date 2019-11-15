f34a48eb8bc9dfa6a8bdddc64dabfdbb6bee9d85
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-15 01:37:29 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,6 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    resp = Faraday.post "https://github.com/login/oauth/access_token"
   end
 end
