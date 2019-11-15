cb14dfd76837266f8123cc1988d63edc50554112
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-15 01:37:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,8 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    resp = Faraday.post "https://github.com/login/oauth/access_token"
+    resp = Faraday.post "https://github.com/login/oauth/access_token" do |req|
+      req.body
+      req.headers['Accept'] = 'application/json'
   end
 end
