2c9c4c962b361ce7a785188a01fbd5f5528f5377
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-15 02:07:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,8 @@ class SessionsController < ApplicationController
     session[:token] = body["access_token"]
 
     user_resp = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_resp.body)
+    session[:username] = user_json["login"]
     redirect_to root_path
   end
 end
