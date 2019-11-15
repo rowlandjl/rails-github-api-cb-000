8bdf50e74449c315fd90646d5fd500032a241df8
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-15 01:40:37 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,8 @@ class SessionsController < ApplicationController
       req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
       req.headers['Accept'] = 'application/json'
     end
+
+    body = JSON.parse(resp.body)
+    session[:token] = body["access_token"]
   end
 end
